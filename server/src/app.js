532b4f1d2ee5e3bad44a4783a75b702f8cf7c0d9
Tao02 <idowutaofeek16@gmail.com>
2026-07-26 02:56:40 +0100

    Create a health route

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import healthRoutes from "./routes/health.routes.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // Routes will be registered here
+app.use("/api/health", healthRoutes);
 
 export default app;
