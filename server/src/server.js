fe70185ade2d1fdd88d58d0e7d7555f916427063
Tao02 <idowutaofeek16@gmail.com>
2026-07-26 04:09:48 +0100

    Connect to Database

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 import http from "http";
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectToDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+connectToDB()
 
 const server = http.createServer(app);
 
