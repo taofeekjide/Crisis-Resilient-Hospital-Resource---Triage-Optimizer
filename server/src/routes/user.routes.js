11611a163198ffa4ef41f8b66ceb9b37473da50f
Tao02 <idowutaofeek16@gmail.com>
2026-07-28 11:43:00 +0100

    Add role authentication middleware

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,13 +1,29 @@
 import { Router } from "express";
 import protect from "../middleware/auth.middleware.js";
+import authorize from "../middleware/role.middleware.js";
 
 const router = Router();
 
+// authenticated user
 router.get("/me", protect, (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Authenticated user.",
     data: req.user,
+  });
+});
+
+router.get("/admin", protect, authorize("Admin"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome Admin!",
+  });
+});
+
+// Admin, Doctor or Nurse (Staffs only)
+router.get("/staff", protect, authorize("Admin", "Doctor"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome Staff Member!",
   });
 });
 
