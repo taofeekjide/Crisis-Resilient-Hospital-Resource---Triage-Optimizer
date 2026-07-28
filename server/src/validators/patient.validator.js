f602d8f4a6ee28ea7f3cdde990ec7e09fad29f5a
Tao02 <idowutaofeek16@gmail.com>
2026-07-28 19:02:49 +0100

    Create patient validation

---
+++ server/src/validators/patient.validator.js
@@ -0,0 +1,64 @@
+import { body } from "express-validator";
+
+export const createPatientProfileValidator = [
+  body("user")
+    .notEmpty()
+    .withMessage("User ID is required.")
+    .isMongoId()
+    .withMessage("Invalid User ID."),
+
+  body("dateOfBirth")
+    .notEmpty()
+    .withMessage("Date of birth is required.")
+    .isISO8601()
+    .withMessage("Date of birth must be a valid date."),
+
+  body("gender")
+    .notEmpty()
+    .withMessage("Gender is required.")
+    .isIn(["Male", "Female", "Other"])
+    .withMessage("Invalid gender."),
+
+  body("bloodGroup")
+    .optional()
+    .isIn(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+    .withMessage("Invalid blood group."),
+
+  body("allergies")
+    .optional()
+    .isArray()
+    .withMessage("Allergies must be an array."),
+
+  body("activeMedications")
+    .optional()
+    .isArray()
+    .withMessage("Active medications must be an array."),
+
+  body("chronicConditions")
+    .optional()
+    .isArray()
+    .withMessage("Chronic conditions must be an array."),
+
+  body("emergencyContact.name")
+    .trim()
+    .notEmpty()
+    .withMessage("Emergency contact name is required."),
+
+  body("emergencyContact.relationship")
+    .trim()
+    .notEmpty()
+    .withMessage("Relationship is required."),
+
+  body("emergencyContact.phone")
+    .trim()
+    .notEmpty()
+    .withMessage("Emergency contact phone is required."),
+
+  body("address.street").optional().trim(),
+
+  body("address.city").optional().trim(),
+
+  body("address.state").optional().trim(),
+
+  body("address.country").optional().trim(),
+];
